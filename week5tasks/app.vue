ea9497a1163c5e0ee15ab49b332cce0ccefee26b
Davym8 <62274264+Davym8@users.noreply.github.com>
2021-08-27 16:21:54 +1000

    Add files via upload

---
+++ week5tasks/app.vue
@@ -0,0 +1,16 @@
+   
+new Vue({
+el: '#senddata',
+data: {
+fname: '',
+lname:'',
+check: false,
+checked: false,
+checkers: false
+},
+methods: {
+    processform: function(){
+        console.log({fname: this.fname, lname: this.lname, check: this.check, checked: this.checked, checkers: this.checkers,})
+    }
+}
+})
